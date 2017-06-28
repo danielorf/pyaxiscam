06b52540404d387a02d5579900e422ac2bec5ab5
Daniel Orf <danielorf@gmail.com>
2017-06-27 21:17:50 -0700

    Initial commit

---
+++ pyaxiscam.py
@@ -0,0 +1,63 @@
+#from tokens import host, username, password
+import requests
+import io
+from PIL import Image
+
+
+class AxisCam:
+
+    def __init__(self, host, username, password):
+        self.base_url = 'http://{}:{}@{}/axis-cgi/'.format(username, password, host)
+
+    def get_supported_VAPIX_version(self):
+        r = requests.get(self.base_url + 'param.cgi?action=list&group=Properties.API.HTTP.Version')
+        print('Return status: ' + str(r.status_code))
+        print(r.content.decode('utf-8'))
+        return (r.status_code, r.content.decode('utf-8'))
+
+    def get_supported_resolutions(self):
+        r = requests.get(self.base_url + 'param.cgi?action=list&group=Properties.Image.Resolution')
+        print('Return status: ' + str(r.status_code))
+        print(r.content.decode('utf-8'))
+
+    def get_supported_image_formats(self):
+        r = requests.get(self.base_url + 'param.cgi?action=list&group=Properties.Image.Format')
+        print('Return status: ' + str(r.status_code))
+        print(r.content.decode('utf-8'))
+
+    def get_default_resolution(self):
+        r = requests.get(self.base_url + 'imagesize.cgi?camera=1')
+        print('Return status: ' + str(r.status_code))
+        print(r.content.decode('utf-8'))
+
+    def get_live_image(self):
+        r = requests.get(self.base_url + 'jpg/image.cgi')
+        print('Return status: ' + str(r.status_code))
+        if r.status_code == 200:
+            f = io.BytesIO(r.content)
+            img = Image.open(f)
+            img.show()
+        return r.status_code
+
+    # TODO
+    # https://stackoverflow.com/questions/21702477/how-to-parse-mjpeg-http-stream-from-ip-camera
+    # def get_MJPEG_stream(self):
+    #     r = requests.get(self.base_url + 'mjpg/video.cgi')
+    #     print('Return status: ' + str(r.status_code))
+    #     print(r.content.decode('utf-8'))
+
+
+
+
+
+
+
+
+# print(base_url + 'jpg/image.cgi')
+# r = requests.get(base_url + 'jpg/image.cgi')
+# print(r.status_code)
+# if r.status_code == 200:
+#     f = io.BytesIO(r._content)
+#     img = Image.open(f)
+#     img.show()
+
